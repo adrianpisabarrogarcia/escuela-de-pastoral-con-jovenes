bb3ac0d077fff7313222ef23cd67532a2a9697a1
Adrián Pisabarro García <adrian.pisabarro.garcia@gmail.com>
2024-12-28 13:13:52 +0100

    Init Project 🚀

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			colors: {
+				'custom-red': '#9C3E45',
+				'custom-blue': '#135476',
+				'custom-yellow-chrome': '#ffeb3b',
+			}
+		},
+	},
+	plugins: [],
+}
